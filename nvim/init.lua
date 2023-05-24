c34f42b2c4208953899379aebfa17bffbca6aeef
dnl <>
2023-05-24 20:28:09 +0300

    gave in to nvchad

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,24 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-local cmd = vim.cmd
+require "core"
 
--- require 'plugins'
-require 'settings'      -- defaults
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-vim.g.run_nostream_default = 1
+if custom_init_path then
+  dofile(custom_init_path)
+end
 
-local utils = require('utils')
+require("core.utils").load_mappings()
 
--- colorscheme
-vim.cmd "set background=light"
-vim.cmd "colorscheme intellij"
-vim.cmd "let g:lightline = { 'colorscheme': 'intellij' }"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- require'lspconfig'.clangd.setup{}
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
